57fea8cc8b7fbfb7c16c85bd076c89c2b9d188b3
Kacper Mól <kacpermol450@gmail.com>
2024-03-19 11:31:59 +0100

    dodaj kolejny konstruktor i funkcjonalnosc kodu oraz nadaj nazwy polom entry

--- zapisywaneListy/zapisywaneListy/DodajPage.xaml.cs
+++ zapisywaneListy/zapisywaneListy/DodajPage.xaml.cs
@@ -12,9 +12,37 @@ namespace zapisywaneListy
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class DodajPage : ContentPage
     {
+        Produkt Produkt;
         public DodajPage()
         {
             InitializeComponent();
+            naglowek.Text = "Dodaj produkt";
+            naglowek.TextColor = Color.Green;
+            przycisk.Text = "Dodaj";
+            przycisk.BackgroundColor = Color.LightGreen;
+            przycisk.Clicked += dodaj_Clicked;
+        }
+        public DodajPage(Produkt produkt)
+        {
+            InitializeComponent();
+            Produkt = produkt;
+            naglowek.Text = "Edytuj produkt";
+            naglowek.TextColor = Color.Orange;
+            przycisk.Text = "Edytuj";
+            przycisk.BackgroundColor = Color.Yellow;
+            przycisk.Clicked += edytuj_Clicked;
+            txtNazwa.Text = Produkt.Nazwa;
+            txtCena.Text = Produkt.Cena.ToString();
+            txtIlosc.Text = Produkt.Ilosc.ToString();
+        }
+
+        private void dodaj_Clicked(object sender, EventArgs e)
+        {
+
+        }
+        private void edytuj_Clicked(object sender, EventArgs e)
+        {
+
         }
     }
 }
